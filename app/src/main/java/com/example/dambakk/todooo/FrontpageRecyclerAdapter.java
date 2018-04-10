e999249d0cd1e2680f88d8f438450341a25c36b3
Christopher Dambakk <chrisd14@uia.no>
2018-04-10 10:14:19 +0200

    Remove items

--- app/src/main/java/com/example/dambakk/todooo/FrontpageRecyclerAdapter.java
+++ app/src/main/java/com/example/dambakk/todooo/FrontpageRecyclerAdapter.java
@@ -1,10 +1,16 @@
 package com.example.dambakk.todooo;
 
+import android.animation.Animator;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -26,6 +32,11 @@ public class FrontpageRecyclerAdapter extends RecyclerView.Adapter<TodoItemViewH
         this.notifyDataSetChanged();
     }
 
+    public void removeItem(int position){
+        items.remove(position);
+        notifyItemRemoved(position);
+    }
+
 
     @NonNull
     @Override
@@ -37,10 +48,42 @@ public class FrontpageRecyclerAdapter extends RecyclerView.Adapter<TodoItemViewH
 
     @Override
     public void onBindViewHolder(@NonNull TodoItemViewHolder holder, int position) {
-        holder.populateItem(items.get(position));
+        TodoItemModel todoItem = items.get(position);
+        holder.populateItem(todoItem);
         holder.clickArea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                holder.doneAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        //Remove from db:
+                        todoItem.setDone(true);
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if( user == null) return;
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
+                                .child("user")
+                                .child(user.getUid())
+                                .child("todoItems")
+                                .child(todoItem.getTimestampCreated())
+                                .child("done");
+                        ref.setValue(true);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                });
                 holder.doneAnimation.setProgress(0f);
                 holder.doneAnimation.setSpeed(1f);
                 holder.doneAnimation.loop(false);
